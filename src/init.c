31cb424dfc50620af101588dd73d4e77838a7f6b
Iaotle <vintersiaotle@gmail.com>
2022-02-13 16:36:29 +0100

    maybe this works

--- src/init.c
+++ src/init.c
@@ -71,9 +71,9 @@ void __attribute__ ((constructor)) _init_anp_netstack() {
     printf("Hello there, I am ANP networking stack!\n");
     _function_override_init();
     // this is the external end, at 10.0.0.5
-    tdev_init();
+    // tdev_init();
     // this is the client end, at 10.0.0.4
-    client_netdev_init();
+    // client_netdev_init();
     // insert and init some default routes about, lo, local delivery, and the gateway
     route_init();
     init_threads();
